9f443ce3bf00cd9fba617b19a7dbb3c61a04dbc3
Enitsaj13 <enitsajformentera.13@gmail.com>
2022-08-08 20:40:16 +0800

    react app from scratch

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Modal from './components/Modal';
+import { AdjustmentsIcon } from '@heroicons/react/outline';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Modal />
+      <AdjustmentsIcon width='20px' />
+    </div>
+  );
+};
+
+export default App;
